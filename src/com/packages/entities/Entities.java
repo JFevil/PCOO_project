76c20c5607dc991ce255637b767a74b8f9510f71
JFevil <flechejeremy@gmail.com>
2024-10-17 17:39:52 +0200

    add repository src

---
+++ src/com/packages/entities/Entities.java
@@ -0,0 +1,14 @@
+package com.packages.entities;
+
+public class Entities {
+
+	int positionX;
+	int positionY;
+	int 
+
+	protected Entities(int positionX, int positionY) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+
+}
---
+++ src/com/packages/entities/Player.java
@@ -0,0 +1,15 @@
+package com.mypackage
+
+public class Player {
+
+	int pv;
+	int attack;
+	int speed;
+
+	public Player(int pv, int attack, int speed) {
+		this.pv = pv;
+		this.attack = attack;
+		this.speed = speed;
+	}
+
+}
