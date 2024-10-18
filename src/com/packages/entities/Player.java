78523982dcd78af12990a495f97f44b3ddd125a6
JFevil <flechejeremy@gmail.com>
2024-10-18 13:01:08 +0200

    maj src

--- src/com/packages/entities/Player.java
+++ src/com/packages/entities/Player.java
@@ -3,7 +3,6 @@ import com.packages.entities.Entities;
 import com.packages.weapons.Weapons;
 import com.packages.equipment.Equipment;
 
-
 public class Player extends Entities {
 
 	Weapons[] weapons;
