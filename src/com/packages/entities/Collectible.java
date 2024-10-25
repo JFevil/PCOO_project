813fcaf3db97a1059cc84f55639142a91a62c93f
JFevil <flechejeremy@gmail.com>
2024-10-25 18:17:54 +0200

    add class Collectible

---
+++ src/com/packages/entities/Collectible.java
@@ -0,0 +1,9 @@
+package com.packages.entities;
+
+public class Collectible extends Entities {
+    
+    public Collectible(int positionX, int positionY, int hP, int attack, int speed, int resistance, double orientation) {
+        super(positionX, positionY, hP, attack, speed, resistance, orientation);
+    }
+
+}
