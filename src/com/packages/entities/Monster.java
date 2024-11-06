b36b5bb4ae3f377a1fe42354bbf5829233bc588e
JFevil <flechejeremy@gmail.com>
2024-11-06 09:47:28 +0100

    update monster

--- src/com/packages/entities/Monster.java
+++ src/com/packages/entities/Monster.java
@@ -1,13 +1,8 @@
 package com.packages.entities;
 
-public class Monster extends Entities implements Updatable {
+public class Monster extends Entities {
 
 	public Monster (int positionX, int positionY, int hP, int attack, int speed, int resistance) {
 		super(positionX, positionY, hP, attack, speed, resistance, 0);
-	}
-
-	@Override
-	public void update() {
-		
 	}
 }
